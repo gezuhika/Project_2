8900531a964162e4daf516663b265f59f2968b9f
Jeffrey Fleer <Jeff@as-MacBook-Pro.local>
2019-07-08 18:25:20 -0600

    again

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(driverRoutes);
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
